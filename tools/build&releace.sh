f3137689683cf642d00e99d5d55f64a534f36aa0
Eito Takase <eito01178@gmail.com>
2023-04-25 23:26:46 +0900

    r2にバンドルしたファイルホスティングしとくのもやめる

--- tools/build&releace.sh
+++ tools/build&releace.sh
@@ -10,9 +10,6 @@ npm version $1
 git push
 git push origin --tags
 npm publish ./
-yarn bundle:esm
-# upload to cloudflare r2
-yarn wrangler r2 object put packages/simple-prompt-executer/index.min.js --file ./lib/bundle/index.min.js
 git checkout master
 git merge develop
 git push
